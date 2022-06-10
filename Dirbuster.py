bd37b24530ec5f1f9f85e89c6b080067613f8f0d
PykeOzori <100375880+PykeOzori@users.noreply.github.com>
2022-06-10 16:12:03 +0200

    Update Dirbuster.py

--- Dirbuster.py
+++ Dirbuster.py
@@ -1,44 +1,72 @@
-import sys
 import pydirbuster
 import requests
 
-website_online = False
-request = None
-URL = input("Website: ")
-try:
-	request = requests.get('http://'+URL)
-	if request.status_code == 301:
-		request = requests.get('https://'+URL)
-		URL = 'https://'+URL
-	else: 
-		URL = 'http://'+URL
+def verify_domain(domain):
+	"""
+	Verifies if a domain can be reached over HTTP or HTTPS and
+	returns the full URL or None if the website is offline.
 
-	if request.status_code == 200:
-		website_online = True
+	Args:
+		domain (string): The domain to verify
+
+	Returns:
+		String|None: URL of online website
+	"""
+
+	url = None
+
+	try:
+		# See if we can reach domain over HTTP or HTTPS
+		request = requests.get('http://'+domain)
+
+		if request.status_code == 301:
+			request = requests.get('https://'+domain)
+			url = 'https://'+domain
+		else: 
+			url = 'http://'+domain
+
+		# Only succeed if the status code is 200
+		if request.status_code != 200:
+			url = None
+	except:
+		print("Domain does not exist")
 	
-except:
-	print("Domain does not exist")
-	sys.exit()
+	return url
 
-if website_online == False:
-	print("Website is not online")
-	sys.exit()
+def clean_results(results):
+	"""
+	Filters out results of Dirbuster for easier data handling.
 
-print(request.status_code)
+	Args:
+		results (dict): Raw dirbuster result
 
+	Returns:
+		dict: Cleaned up results
+	"""
 
-def main():
-	print("[Pybuster test]")
+	# TODO: Only get 200 status codes or clean up results
+	return results
+
+def run(url):
+	"""
+	Run the dirbuster library (https://pypi.org/project/pydirbuster/) and will prompt for
+	user imput.
+
+	Args:
+		url (string): The URL to run dirbuster on
+
+	Returns:
+		dict: Dirbuster scan results
+	"""
+
+	# TODO: See if file exists or use try/catch
+	wordlist = input("Path to wordlist: ")
 
 	webbuster = pydirbuster.Pybuster(
-		url=URL,
-		wordfile=input("Path to wordlist: "),
+		url=url,
+		wordfile=wordlist,
 		exts=[] # php, html
 	)
-	webbuster.Run
+	webbuster.Run()
 
-
-	print(webbuster.results)
-
-if __name__ == "__main__":
-	main()
+	return clean_results(webbuster.results)
